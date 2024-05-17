de1398e100c3c4523d5ed828e97a23e92be84ba6
Mvke-IT <michael.duarte09@gmail.com>
2024-05-17 09:17:46 -0400

    Trying a new code

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,28 +1,26 @@
-class Currency {
+export default class Currency {
   constructor(code, name) {
-    this.code = code;
-    this.name = name;
+    this._code = code;
+    this._name = name;
   }
 
   get code() {
     return this._code;
   }
 
-  set code(code) {
-    this._code = code;
-  }
-
   get name() {
     return this._name;
   }
 
-  set name(name) {
-    this._name = name;
+  set code(newCode) {
+    this._code = newCode;
+  }
+
+  set name(newName) {
+    this._name = newName;
   }
 
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
 }
-
-export default Currency;
