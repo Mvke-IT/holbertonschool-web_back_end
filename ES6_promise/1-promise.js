4f3f3f88c384caaa60c9c8f52662c830c60cabd9
Mvke-IT <michael.duarte09@gmail.com>
2024-05-17 08:00:19 -0400

    back to the first code

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,15 +1,14 @@
 function getFullResponseFromAPI(success) {
-  eturn new Promise((resolve, reject) => {
-
-    if (success) {
-      resolve({
-        status: 200,
-        body: 'Success',
-      });
-    } else {
-      reject(new Error('The fake API is not working currently'));
-    }
-  });
-}
- 
+    return new Promise((resolve, reject) => {
+      if (success) {
+        resolve({
+          status: 200,
+          body: 'Success',
+        });
+      } else {
+        reject(new Error('The fake API is not working currently'));
+      }
+    });
+  }
+  
   export default getFullResponseFromAPI;
