57da36819d0bb05b4a8925c2c257218d71bcdf7a
Mvke-IT <michael.duarte09@gmail.com>
2024-05-17 08:28:30 -0400

    indentation

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -2,15 +2,15 @@ function handleResponseFromAPI(promise) {
   return promise.then(() => {
     try {
       return {
-       status: 200,
-       body: 'success',
-     };
-   } catch (error) {
-     throw new Error('The fake API is not working currently');
-   }
- })
-   .catch(() => new Error('The fake API is not working currently'))
-   .finally(() => console.log('Got a response from the API'));
+        status: 200,
+        body: 'success',
+      };
+    } catch (error) {
+      throw new Error('The fake API is not working currently');
+    }
+  })
+    .catch(() => new Error('The fake API is not working currently'))
+    .finally(() => console.log('Got a response from the API'));
 }
- 
+
 export default handleResponseFromAPI;
