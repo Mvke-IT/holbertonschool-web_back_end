a900d70d1f1a7903a6b48b4b1f95511a58200b3b
Mvke-IT <michael.duarte09@gmail.com>
2024-05-17 08:35:23 -0400

    created a shorter version for it

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,16 +1,8 @@
-function handleResponseFromAPI(promise) {
-    return promise.then(() => {
-      try {
-        const result = {
-          status: 200,
-          body: 'success',
-        };
-        console.log('Got a response from the API');  // Moved inside .then()
-        return result; 
-      } catch (error) {
-        throw new Error('The fake API is not working currently');
-      }
-    })
-      .catch(() => new Error('The fake API is not working currently')); 
+export default function handleResponseFromApi(promise) {
+    return promise.then(() => (
+      { status: 200, body: 'success' }))
+      .catch(() => new Error())
+      .finally(() => {
+        console.log('Got a response from the API');
+      });
   }
-  
