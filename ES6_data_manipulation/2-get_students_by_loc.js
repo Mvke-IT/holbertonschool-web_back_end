cb1cd6ffd29dadab6cebd777ab02c973febc4ce2
Mvke-IT <michael.duarte09@gmail.com>
2024-05-28 09:06:59 -0400

    fixing indentation

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,6 @@
 export default function getStudentsByLocation(listStudents, city) {
-    if (!Array.isArray(listStudents)) {
-  return [];
+  if (!Array.isArray(listStudents)) {
+    return [];
   }
-return listStudents.filter((student) => (student.location === city));
+  return listStudents.filter((student) => (student.location === city));
 }
